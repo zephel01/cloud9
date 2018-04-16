17196cbc51fc754b0b3d3d112830d1ade78791b1
EC2 Default User <ec2-user@ip-172-31-0-57.eu-west-1.compute.internal>
2018-04-16 23:53:14 +0900

    add script

---
+++ python_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 開発環境インストール
+sudo yum install -y gcc gcc-c++ make git openssl-devel bzip2-devel zlib-devel readline-devel sqlite-devel
+
+# pyenv install
+
+sudo /usr/bin/git clone https://github.com/yyuu/pyenv.git /usr/bin/.pyenv
+
+cd /usr/bin/.pyenv
+sudo mkdir shims
+sudo mkdir versions
+
+sudo chown -R ec2-user:ec2-user /usr/bin/.pyenv
+
+cd ~
+sed -i -e 's/alias python/#alias python/g' ~/.bashrc
+
+cat ~/cloud9/bashrc_add >> ~/.bashrc
+
+source ~/.bash_profile
+
+pyenv install 3.6.5
+pyenv global 3.6.5
+
+pip install --upgrade pip
+
+pip install ccxt
+pip install simplejson
+pip install pybitflyer
+pip install requests
+pip install datetime
