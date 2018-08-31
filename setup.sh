4e59b95ce99784db5ac7c01dc238a3e72ba3c984
zephel01 <zephel01@gmail.com>
2018-08-31 22:21:01 +0900

    script change

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 sudo cp -p /etc/sysctl.conf /etc/sysctl.conf.org
-sudo cp ~/cloud9/sysctl.conf /etc/sysctl.conf
 
+cp ~/cloud9/sysctl.conf /tmp
+cp ~/cloud9/limits_add /tmp
 
-sudo cat ~/cloud9/limits_add >> /etc/security/limits.conf
-
+sudo cp /tmp/sysctl.conf /etc/sysctl.conf
+sudo bash -c "cat /tmp/limits_add >> /etc/security/limits.conf"
 
 sudo /sbin/sysctl -p
