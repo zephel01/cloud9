8fd3d0e78ae5a7c325580110ad5a4c2259a39722
zephel01 <zephel01@gmail.com>
2018-09-25 02:19:36 +0900

    chrome driver install

---
+++ chrome_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip install selenium
+
+curl https://intoli.com/install-google-chrome.sh | bash
+
+sudo yum -y install ipa-gothic-fonts
+
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+
+unzip chromedriver_linux64.zip 
+
+chmod +x chromedriver
+
+mv chromedriver /usr/local/bin
