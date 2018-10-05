9cfe75c5b24d59931a57fc9d8b54c8d7e1c47b60
zephel01 <zephel01@gmail.com>
2018-10-05 20:56:27 +0900

    add EOF

--- setup.sh
+++ setup.sh
@@ -5,10 +5,13 @@ sudo yum -y install htop
 sudo cp -p /etc/sysctl.conf /etc/sysctl.conf.org
 
 cp ~/cloud9/sysctl.conf /tmp
-cp ~/cloud9/limits_add /tmp
 
 sudo cp /tmp/sysctl.conf /etc/sysctl.conf
-sudo bash -c "cat /tmp/limits_add >> /etc/security/limits.conf"
+
+sudo cat << 'EOF' >> /etc/security/limits.conf
+* soft nofile 65536
+* hard nofile 65536
+EOF
 
 sudo /sbin/sysctl -p
 ulimit -a
