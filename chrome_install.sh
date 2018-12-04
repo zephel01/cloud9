828bb789e3d73a2646f4faf5891918a48b687f38
EC2 Default User <ec2-user@ip-172-31-28-251.ap-southeast-1.compute.internal>
2018-12-04 16:29:40 +0000

    修正

--- chrome_install.sh
+++ chrome_install.sh
@@ -2,14 +2,9 @@
 
 pip install selenium
 
-sudo yum install -y curl unzip wget gcc gcc-c++ make git openssl-devel bzip2-devel zlib-devel readline-devel sqlite-devel expect
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-sudo yum localinstall -y google-chrome-stable_current_x86_64.rpm
-rm -f google-chrome-stable_current_x86_64.rpm
+curl https://intoli.com/install-google-chrome.sh | bash
 
 wget https://chromedriver.storage.googleapis.com/2.44/chromedriver_linux64.zip
 
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/local/bin
-rm -f chromedriver_linux64.zip
