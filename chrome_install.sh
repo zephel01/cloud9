8176cf903cd1a69df806d8ad9c8578c1351408ab
zephel01 <zephel01@gmail.com>
2018-12-05 00:36:11 +0900

    修正

--- chrome_install.sh
+++ chrome_install.sh
@@ -2,14 +2,14 @@
 
 pip install selenium
 
-curl https://intoli.com/install-google-chrome.sh | bash
+sudo yum install -y curl unzip wget gcc gcc-c++ make git openssl-devel bzip2-devel zlib-devel readline-devel sqlite-devel expect
 
-sudo yum -y install ipa-gothic-fonts
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo yum localinstall -y google-chrome-stable_current_x86_64.rpm
+rm -f google-chrome-stable_current_x86_64.rpm
 
-wget https://chromedriver.storage.googleapis.com/2.42/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/2.44/chromedriver_linux64.zip
 
-unzip chromedriver_linux64.zip 
-
-chmod +x chromedriver
-
+unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/local/bin
+rm -f chromedriver_linux64.zip
