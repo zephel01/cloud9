fe8b26a76f4f9e493335634f392c6cde89a2ae7b
EC2 Default User <ec2-user@ip-172-31-25-103.ap-southeast-1.compute.internal>
2018-12-04 18:07:56 +0000

    修正

--- chrome_install.sh
+++ chrome_install.sh
@@ -2,9 +2,12 @@
 
 pip install selenium
 
+sudo rpm -ivh mesa-libwayland-egl-18.0.5-3.el7.x86_64.rpm
 curl https://intoli.com/install-google-chrome.sh | bash
 
 wget https://chromedriver.storage.googleapis.com/2.44/chromedriver_linux64.zip
 
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/local/bin
+
+cd /usr/bin && sudo ln -s /opt/google/chrome/google-chrome google-chrome
