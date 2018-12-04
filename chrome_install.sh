7814c884a6df444fcb7e8f8ddad86e20334f6e29
EC2 Default User <ec2-user@ip-172-31-28-56.ap-southeast-1.compute.internal>
2018-12-04 18:41:11 +0000

    修正

--- chrome_install.sh
+++ chrome_install.sh
@@ -11,4 +11,4 @@ wget https://chromedriver.storage.googleapis.com/2.44/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/local/bin
 
-#cd /usr/bin && sudo ln -s /opt/google/chrome/google-chrome google-chrome
+cd /usr/bin && sudo ln -s /opt/google/chrome/google-chrome google-chrome
