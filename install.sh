e70dc56a9fd2025489432404b595bf2383abe6c2
Hideki Yamamoto <h.yamamoto@bitpoint.co.jp>
2021-05-29 20:20:13 +0900

    install anyenv

---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# 開発環境インストール
+sudo yum install -y openssl-devel bzip2-devel zlib-devel readline-devel sqlite-devel glibc-static
+sudo yum install -y gcc-c++ openssl-devel git readline-devel
+sudo yum groupinstall -y development
+
+#amazonlinux2 epel
+sudo amazon-linux-extras install epel
+#centos epel
+sudo yum -y install epel-release
+
+chsh -s /bin/bash
+
+cd ~
+sed -i -e 's/alias python/#alias python/g' ~/.bashrc
+
+## install anyenv
+git clone https://github.com/anyenv/anyenv ~/.anyenv
+
+## export anyenv
+cat << 'EOF' >> ~/.bash_profile
+# export anyenv
+export PATH="$HOME/.anyenv/bin:$PATH"
+eval "$(anyenv init - --no-rehash)"
+EOF
+
+~/.anyenv/bin/anyenv init
+exec $SHELL -l
+anyenv install --init
+
+## install pyenv
+anyenv install pyenv
+
+## export pyenv
+cat << 'EOF' >> ~/.bash_profile
+export PYENV_ROOT="$HOME/.anyenv/envs/pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+EOF
+
+exec $SHELL -l
+
+pyenv install 3.6.10
+pyenv global 3.6.10
+
+pyenv rehash
+
+pip install --upgrade pip
+
+## pip install
+pip install -r ~/cloud9/requirements.txt
+
+## ruby install
+anyenv install rbenv
+
+## export rbenv
+cat << 'EOF' >> ~/.bashrc
+export RBENV_ROOT="$HOME/.anyenv/envs/rbenv"
+export PATH="$RBENV_ROOT/bin:$PATH"
+eval "$(rbenv init -)"
+EOF
+
+exec $SHELL -l
+
+rbenv install 2.7.1
+rbenv global 2.7.1
+
